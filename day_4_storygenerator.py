8c577f0512bd157d5c7ac80eed34ae99ec967364
Aditya <subhaiyash@gmail.com>
2022-10-16 15:23:27 +0530

    changes

--- day_4_storygenerator.py
+++ day_4_storygenerator.py
@@ -5,5 +5,5 @@ who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle', 'a cat']
 name = ['Adi', 'Vishal', 'Vikash', 'Daniel', 'Nikhil', 'Lukesh']
 residence =  ['Barcelona','India', 'Germany', 'Venice', 'England' ]
 went = ['cinema', 'university', 'seminar','stadium', 'college','market']
-happened = ['made a lot of enemies', 'punched him in the face', 'eats a burger', 'founds a treasure', 'fought of a bull', 'kissed a girl']
-print(random.choice(when)+ ', '+ random.choice(who) + ' that lived in '+ random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+happened = ['made a lot of enemies.', 'punched him in the face.', 'eats a burger.', 'founds a treasure.', 'fought of a bull.', 'kissed a girl.']
+print(random.choice(when)+ ', '+ random.choice(name) + ' who lived in '+ random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))

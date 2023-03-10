b8d6d68fa06733d2e2149ca0687cbd9ad6779dfd
Aditya <subhaiyash@gmail.com>
2023-03-10 10:50:20 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
 from cmath import pi
 import math
-radi = input("Enter radius of a circle: ")
-radi.pi
-
+print("Select one of the following :")
+print(" 1) Square \n 2) Triangle ")
+opt = int(input("Enter a number:" ))
+if opt ==1:
+    side = int(input("Enter length of a side: "))
+    print(side**2)
+elif opt ==2:
+    base = int(input("Enter length of base: "))
+    height = int(input("Enter length of height: "))
+    print(1/2 * base * height)
+else:
+    print("error")
